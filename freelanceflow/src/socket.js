7ae050786b731dd956b31c3082bc0cc2bed7e878
madhu9787 <madhumitha805632@gmail.com>
2026-02-11 09:09:40 +0530

    fixed

--- freelanceflow/src/socket.js
+++ freelanceflow/src/socket.js
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:5000', {
+export const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
 });
-
-
